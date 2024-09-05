15638e4d863a039e801bff2893a85f409c0a980c
johnburitto <79087305+johnburitto@users.noreply.github.com>
2024-09-05 17:52:11 +0300

    Lab 1

---
+++ Reengineering/Good/HumanBodyMassIndex.cs
@@ -0,0 +1,29 @@
+﻿namespace Reengineering.Good
+{
+    public class HumanBodyMassIndex
+    {
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public float Index => Weight / (Height * Height);
+
+        public string Result()
+        {
+            if (Index >= 18.5 & Index < 25)
+            {
+                return "Norm";
+            }
+            else if (Index >= 25 & Index < 30)
+            {
+                return "Warning!";
+            }
+            else if (Index >= 30)
+            {
+                return "Fat";
+            }
+            else
+            {
+                return "Deficit";
+            }
+        }
+    }
+}
--- Reengineering/Program.cs
+++ Reengineering/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Reengineering.Good;
+
+var index = new HumanBodyMassIndex
+{
+    Weight = 80,
+    Height = 1.52f
+};
+
+Console.WriteLine(index.Result());
